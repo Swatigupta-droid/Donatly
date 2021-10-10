810b51583ef0e98c207cfdf65f4eace776e14459
Abhinav Aggrawal <abhinavagrawal101@gmail.com>
2021-10-10 17:30:17 +0530

    backend

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ const contactContent = "";
 
 const app = express();
 
-app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+app.set('view engine', 'ejs');;
+
 
 // ADD your Username and Password for mongoDB Atlas then
 mongoose.connect("mongodb+srv://admin-"+process.env.DB_NAME + ":" + process.env.DB_PASS + "abhi@cluster0.4ady3.mongodb.net/DonatelyDB?retryWrites=true&w=majority", {useNewUrlParser: true});
@@ -30,77 +31,112 @@ const contactSchema = {
 
 const Contact = mongoose.model("Contact", contactSchema);
 
-
 app.get("/",function(req,res){
-  res.render("./home/index.ejs");
+  // res.render("./home/index.ejs");
+  res.render("./home/index")
 });
 
-app.get("/home", function(req, res){
+app.get("/donor",function(req,res){
+  res.render("./donor/index")
+});
 
-  Post.find({}, function(err, posts){
-    res.render("home", {
-      startingContent: homeStartingContent,
-      posts: posts
-      });
-  });
+app.get("/recipient",function(req,res){
+  res.render("./recipient/index")
+});
+
+app.get("/register",function(req,res){
+  res.render("./anirudh/form")
+});
+
+app.get("/map-blood",function(req,res){
+  res.render("./shreya/blood-map/index")
+});
+
+app.get("/map-organ",function(req,res){
+  res.render("./shreya/organ-map/index")
+});
+
+app.get("/bank",function(req,res){
+  res.render("./shreya/bank/index")
+});
+
+app.get("/events-news",function(req,res){
+  res.render("./Events-news/Events")
+});
+
+app.get("/donations",function(req,res){
+  res.render("./Donations/index")
 });
 
 
 
-app.get("/compose", function(req, res){
-  res.render("compose");
-});
+
+// app.get("/home", function(req, res){
+//
+//   Post.find({}, function(err, posts){
+//     res.render("home", {
+//       startingContent: homeStartingContent,
+//       posts: posts
+//       });
+//   });
+// });
 
 
-app.get("/contact", function(req, res){
-  res.render("contact" ,{
-    responseHead: "Failure",
-    responseBody: "Please try again" 
-  });
-});
 
-app.post("/compose", function(req, res){
-
-  const post = new Post({
-    title: req.body.postTitle,
-    content: req.body.postBody,
-    type: req.body.postType,
-    img: req.body.postImg
-  });
+// app.get("/compose", function(req, res){
+//   res.render("compose");
+// });
 
 
-  post.save(function(err){
-    if(!err){
-      res.redirect("/compose");
-    }
+// app.get("/contact", function(req, res){
+//   res.render("contact" ,{
+//     responseHead: "Failure",
+//     responseBody: "Please try again" 
+//   });
+// });
+
+// app.post("/compose", function(req, res){
+
+//   const post = new Post({
+//     title: req.body.postTitle,
+//     content: req.body.postBody,
+//     type: req.body.postType,
+//     img: req.body.postImg
+//   });
+
+
+//   post.save(function(err){
+//     if(!err){
+//       res.redirect("/compose");
+//     }
     
-  });
-});
+//   });
+// });
 
-app.post("/contact", function(req, res){
+// app.post("/contact", function(req, res){
 
-  const contact = new Contact({
-    name: req.body.contactName,
-    email: req.body.contactEmail,
-    reason: req.body.contactReason,
-    detail: req.body.ContactBody
-  });
+//   const contact = new Contact({
+//     name: req.body.contactName,
+//     email: req.body.contactEmail,
+//     reason: req.body.contactReason,
+//     detail: req.body.ContactBody
+//   });
 
-  contact.save(function(err){
-    if (!err){
-      res.render("contact" ,{
-      responseHead: "Sucess",
-      responseBody: "we will revert back to you within 7 working days" 
-    });
-  }
-  else{
-    res.render("contact" ,{
-      responseHead: "Failure",
-      responseBody: "Please try again" 
-    });
-  }
-  });
-});
+//   contact.save(function(err){
+//     if (!err){
+//       res.render("contact" ,{
+//       responseHead: "Sucess",
+//       responseBody: "we will revert back to you within 7 working days" 
+//     });
+//   }
+//   else{
+//     res.render("contact" ,{
+//       responseHead: "Failure",
+//       responseBody: "Please try again" 
+//     });
+//   }
+//   });
+// });
 
 
 app.get("/about", function(req, res){
---
+++ public/js/form.js
@@ -0,0 +1,22 @@
+// const checkbox = document.querySelector('.my-form input[type="checkbox"]');
+// const btns = document.querySelectorAll(".my-form button");
+
+// checkbox.addEventListener("change", function() {
+//   const checked = this.checked;
+//   for (const btn of btns) {
+//     checked ? (btn.disabled = false) : (btn.disabled = true);
+//   }
+// });
+
+// function calculateBMI(){
+//     form.preventDefault();
+//     var height;
+//     var weight;
+//     var bmi;
+//     height = document.getElementById("height").value;
+//     weight = document.getElementById("weight").value;
+//     height /= 100;
+//     bmi = (weight/height*height);
+//     console.log(bmi)
+//     document.getElementById("output").innerHTML = bmi;
+// }
